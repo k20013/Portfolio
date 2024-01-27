67427ee6f6caa50822d1dda6676c4ad0cba6f658
CalebBravo <calebphone72@gmail.com>
2024-01-27 17:14:27 -0300

    Projects view es funcional

--- portfolio/src/components/project/project.jsx
+++ portfolio/src/components/project/project.jsx
@@ -2,38 +2,50 @@ import styles from './project.module.css';
 
 import imagenDePrueba from '../../assets/imagen_de_prueba.jpg'
 
-const data = {
-    image: imagenDePrueba,
-    name: "Portfolio",
-    description: "Portafolio personal",
-    urlGithub: "Github Url",
-    url: "Url"
-}
+const projectsData = [
+    {
+        image: imagenDePrueba,
+        name: "Portfolio",
+        description: "Portafolio personal",
+        urlGithub: "Github Url",
+        url: "Url"
+    },
+    {
+        image: imagenDePrueba,
+        name: "Portfolio",
+        description: "Portafolio personal",
+        urlGithub: "Github Url",
+        url: "Url"
+    }
+];
+
+const cardsConstructor = projectsData.map(data => 
+    <div class={styles.card}>
+        <div class={styles.card2}>
+                    
+            {data.image && <img src={data.image} alt="" className={styles.cardImg} />}
+
+            <div className={styles.cardInformation}>
+
+                <h5 className={styles.cardName}>{data.name}</h5>
+
+                <p className={styles.cardDescription}>{data.description}</p>
+
+                <a href={data.urlGithub}>Github repository</a>
+
+                {data.url && <a href={data.url}>Link to this project</a>}
+                
+            </div>
+        </div>
+    </div>
+)
 
 const Project = () => {
 
     return(
         <section className={styles.cardsContainer}>
 
-            <div class={styles.card}>
-                <div class={styles.card2}>
-                    
-                {data.image && <img src={data.image} alt="" className={styles.cardImg} />}
-
-                <div className={styles.cardInformation}>
-
-                    <h5 className={styles.cardName}>{data.name}</h5>
-
-                    <p className={styles.cardDescription}>{data.description}</p>
-
-                    <a href={data.urlGithub}>Github repository</a>
-
-                    {data.url && <a href={data.url}>Link to this project</a>}
-                
-                </div>
-
-                </div>
-            </div>
+            {cardsConstructor}
 
         </section>
     )
@@ -51,6 +63,20 @@ export default Project
 
 /*<div class={styles.card}>
                 <div class={styles.card2}>
+                    
+                {data.image && <img src={data.image} alt="" className={styles.cardImg} />}
+
+                <div className={styles.cardInformation}>
+
+                    <h5 className={styles.cardName}>{data.name}</h5>
+
+                    <p className={styles.cardDescription}>{data.description}</p>
+
+                    <a href={data.urlGithub}>Github repository</a>
+
+                    {data.url && <a href={data.url}>Link to this project</a>}
+                
+                </div>
 
                 </div>
             </div>*/
