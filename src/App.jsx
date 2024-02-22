6aa718d0cfd64d828cd8907709b7f1e20cc3e1cc
CalebBravo <calebphone72@gmail.com>
2024-02-22 15:00:17 -0300

    Definición del layout del portafolio responsive.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,36 @@
-import './App.css'
+import './App.css';
 
 // Views
-import Landing from './views/landing/landing';
-import About from './views/about/about';
-import Projects from './views/projects/projects';
+//import About from './views/about/about';
+//import Projects from './views/projects/projects';
 
 // Components
-import NavBar from './components/navBar/navBar';
-import Footer from './components/footer/footer';
+//import NavBar from './components/navBar/navBar';
+//import Footer from './components/footer/footer';
 
-const retFunc = (x) => x;
+const retFunc = (x) => x; //Lo uso para retornar cadenas.
 
 function App() {
 
-  return (
-    <div className={retFunc("appContainer")}>
-      <NavBar />
-      <Landing />
-      <About />
-      <Projects />
-      <Footer />
+  return (      
+    <div>
+
+      <div id={retFunc("home")}>
+
+        <div class={retFunc("module-1 title")}>Titulo principal</div>
+        
+        <div class={retFunc("module-1 about")}>About</div>
+
+        <div class={retFunc("module-1 contact")}>Contacto</div>
+
+        <div class={retFunc("module-1 projects")}>Projectos</div>
+
+        <div class={retFunc("module-1 widget-1")}>widget núm. 1</div>
+
+        <div class={retFunc("module-1 widget-2")}>widget núm. 2</div>
+
+      </div>
+      
     </div>
   )
 }
